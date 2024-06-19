10758fff96c9475f72e088ba858cd5767592f03a
Toann111 <139240230+Toann111@users.noreply.github.com>
2024-06-19 22:05:27 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+const hostname = "localhost";
+const port = 8017;
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Toan , I'am running sever at http://${hostname}:${port}/`);
+});
