154ecad8ee5a38b0966914a99409706b75510511
Toann111 <139240230+Toann111@users.noreply.github.com>
2024-06-26 15:09:32 +0700

    middlerwares

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,19 +1,24 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
-    res.status(StatusCodes.CREATED).json({ massage: 'Post from Controller: Api create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+    // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
+    throw new ApiError(StatusCodes.BAD_GATEWAY, 'test error')
+
+    // res.status(StatusCodes.CREATED).json({ massage: 'Post from Controller: Api create new board' })
+  } catch (error) { next(error) }
 }
 export const boardController = {
   createNew
--- src/validations/exampleValidation.js
+++ src/validations/exampleValidation.js
@@ -5,6 +5,7 @@
  */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async(req, res, next) => {
   const correctCondition = Joi.object({
@@ -17,13 +18,12 @@ const createNew = async(req, res, next) => {
     description: Joi.string().required().min(3).max(256).trim().strict()
   })
   try {
-
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    // const errorMessage = new Error(error).message
+    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next( new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
