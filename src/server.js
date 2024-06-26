692d11ab4ea8c93aca3f408b37de549418767c8a
Toann111 <139240230+Toann111@users.noreply.github.com>
2024-06-26 15:34:14 +0700

    moi truong dev & prod

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, CLOSE_DB} from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
