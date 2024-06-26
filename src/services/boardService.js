04cd4afb8fb836f2e7d1c198f7472513e430acaf
Toann111 <139240230+Toann111@users.noreply.github.com>
2024-06-26 19:31:09 +0700

    api

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,28 @@
+
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+
+const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const newBoard = {
+      ...reqBody,
+      slug: slugify(reqBody.title)
+    }
+    const createBoard = await boardModel.createNew(newBoard)
+
+    const getNewBoard = await boardModel.findOneById(createBoard.insertedId)
+
+    return getNewBoard
+  } catch (error) { throw error }
+}
+
+export const boardService = {
+  createNew
+}
--- src/validations/exampleValidation.js
+++ src/validations/exampleValidation.js
@@ -9,12 +9,7 @@ import ApiError from '~/utils/ApiError'
 
 const createNew = async(req, res, next) => {
   const correctCondition = Joi.object({
-    title: Joi.string().required().min(3).max(50).trim().strict().massages({
-      'any.required': 'Title is required',
-      'string.empty': 'Title is not allowed to be empty',
-      'string.min': 'Title min 3 chars',
-      'string.trim': 'Title must  not have leading or trailing whitespace '
-    }),
+    title: Joi.string().required().min(3).max(50).trim().strict(),
     description: Joi.string().required().min(3).max(256).trim().strict()
   })
   try {
