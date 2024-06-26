afdc89bf4b99a8f68fb94160b00267a66e007c56
Toann111 <139240230+Toann111@users.noreply.github.com>
2024-06-26 19:52:56 +0700

    board detail

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -46,10 +46,19 @@ const findOneById = async (id = number) => {
     throw new Error(error)
   }
 }
+const getDetails = async (id = number) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 
 export const boardModel = {
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
   createNew,
-  findOneById
+  findOneById,
+  getDetails
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 
 /**
  * Updated by trungquandev.com's author on August 17 2023
@@ -7,6 +8,8 @@
 
 import { slugify } from '~/utils/formatters'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -22,7 +25,16 @@ const createNew = async (reqBody) => {
     return getNewBoard
   } catch (error) { throw error }
 }
-
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found! .')
+    }
+    return board
+  } catch (error) { throw error }
+}
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
